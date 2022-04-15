3bf3e4e5c65b306dc2c3cc6451251f7cf7513d3e
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2022-04-15 16:44:06 +0600

    01_BAse_modele log summary

--- src/01_base_model_creation.py
+++ src/01_base_model_creation.py
@@ -7,6 +7,7 @@ from src.utils.common import read_yaml, create_directories
 import random
 import tensorflow as tf
 import numpy as np
+import io
 
 
 STAGE = "Creating base model" ## <<< change stage name 
@@ -55,7 +56,18 @@ def main(config_path):
     METRICS = ["accuracy"]
 
     model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS) 
+    
     model.summary()
+    
+    ## log our model summary information in logs
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    # model.summary()
+    logging.info(f"base model summary: \n{_log_model_summary(model)}")
     
     ## Train the model
     history = model.fit(
